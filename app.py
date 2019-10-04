5362e2797c6a4dca2703890a3690a0d103da96a8
Chudier <chudierp@gmail.com>
2019-10-03 17:15:44 -0700

    Try again

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
@@ -76,6 +76,6 @@ def playlists_delete(playlist_id):
         
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
     # env/lib/python3.7/site-packages/flask/app.py
